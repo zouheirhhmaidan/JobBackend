1f90daeebaebe96f97d9d459b314ea7805f47154
zouheirhhmaidan <104518498+zouheirhhmaidan@users.noreply.github.com>
2024-09-29 14:57:09 +0000

    installations

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+// const axios = require("axios")
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const routes = require("./Routes/routes");
+
+// app.use("/api", routes);
+
+app.listen(process.env.PORT, (req, res) => {
+  console.log("Connected to database on " + process.env.PORT);
+});
